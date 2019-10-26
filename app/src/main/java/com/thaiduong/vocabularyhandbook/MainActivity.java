112bac4307b7c0788a2a3a552fa6acfdc2030286
Thai Duong Vu <vu.t.duong01@gmail.com>
2019-10-26 16:14:40 -0400

    Initial commit

---
+++ app/src/main/java/com/thaiduong/vocabularyhandbook/MainActivity.java
@@ -0,0 +1,20 @@
+package com.thaiduong.vocabularyhandbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onSaveButtonClicked(View view)
+    {
+
+    }
+}
